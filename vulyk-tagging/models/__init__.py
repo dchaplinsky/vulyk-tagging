baa3c3f50b73a2d71925c3317e712e805dbac221
Volodymyr Hotsyk <gotsyk@gmail.com>
2015-03-16 10:44:00 +0200

    Added configure plugin method

---
+++ vulyk-tagging/models/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding=utf-8 -*-
+import logging
+
+from werkzeug.utils import import_string
+
+
+logger = logging.getLogger(__name__)
+
+
+def configure(self_settings):
+    """
+    Getting plugin's default settings, overwriting them with settings
+    from local_settings.py, returns dict of settings
+    """
+    settings = {}
+    try:
+        local_settings = import_string('vulyk.local_settings')
+        for attr in dir(self_settings):
+            settings[attr] = getattr(self_settings, attr)
+        for attr in dir(local_settings):
+            if attr in dir(self_settings):
+                settings[attr] = getattr(local_settings, attr)
+    except Exception as e:
+        logger.warning(e)
+
+    return settings
