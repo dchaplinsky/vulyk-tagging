b35a55a846fa7997bebb01cff54df532a35cad13
Volodymyr Hotsyk <gotsyk@gmail.com>
2015-03-21 00:28:13 +0200

    Added blank settings for assets

--- vulyk_tagging/settings.py
+++ vulyk_tagging/settings.py
@@ -1,1 +1,9 @@
 TASK_NAME = 'TaggingTask'
+
+JS_ASSETS = ['']
+JS_ASSETS_OUTPUT = 'scripts/vulyk-tagging.js'
+JS_ASSETS_FILTERS = 'yui_js'
+
+CSS_ASSETS = ['']
+CSS_ASSETS_OUTPUT = 'styles/vulyk-tagging.css'
+CSS_ASSETS_FILTERS = 'yui_css'
