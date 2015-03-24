79701fa1a7bec5559dce092c06a2056f4c65bad0
Dmitry Gambal <dgambal@conversionscience.co.uk>
2015-03-25 01:00:57 +0200

    Unified ```task_type``` resolving due to https://github.com/mrgambal/vulyk/commit/8ad16ddd856437629a766b3187f682a4365a7481

--- vulyk_tagging/models/task_types.py
+++ vulyk_tagging/models/task_types.py
@@ -1,1 +1,26 @@
 # -*- coding: utf-8 -*-
+from vulyk.models.task_types import AbstractTaskType
+
+from vulyk_tagging.models.tasks import TaggingAnswer, TaggingTask
+
+
+class TaggingTaskType(AbstractTaskType):
+    """
+    Tagging Task to work with Vulyk.
+    """
+    answer_model = TaggingAnswer
+    task_model = TaggingTask
+
+    template = "base.html"
+    helptext_template = "help.html"
+    type_name = "tagging_task"
+
+    redundancy = 3
+
+    JS_ASSETS = ["static/scripts/keymaster.js",
+                 "static/scripts/handlebars.js",
+                 "static/scripts/bootstrap-select.js",
+                 "static/scripts/base.js"]
+
+    CSS_ASSETS = ["static/styles/bootstrap-select.css",
+                  "static/styles/base.css"]
