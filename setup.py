33366a972849f7eecf7a9f1569e938c3f1269704
mrgambal <xbreeder59x@gmail.com>
2016-05-09 18:59:22 +0300

    Bunch of fixes related to migration onto python3
    ADD: forgotten libraries to requirements list;
    FIX: tests main module to be compatible with py34 unittest loader;
    FIX: pep8-ification;

--- setup.py
+++ setup.py
@@ -32,8 +32,7 @@ setup(
     packages=[
         'vulyk_tagging',
         'vulyk_tagging.models',
-        'vulyk_tagging.static',
-        'vulyk_tagging.views'
+        'vulyk_tagging.static'
     ],
     package_dir={'vulyk_tagging':
                  'vulyk_tagging'},
@@ -48,11 +47,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     scripts=['bin/convert_corpus.py'],
